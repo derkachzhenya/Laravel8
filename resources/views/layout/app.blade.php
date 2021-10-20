c572b02f8fc37b276973bd492f1d0eefed421e95
Yevhen <zheka4573@gmail.com>
2021-10-20 16:56:40 +0300

    v0.5

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="css/app.css">
+
+</head>
+
+<body>
+@yield('content')
+    <script src="/js/app.js"></script>
+</body>
+
+</html>
